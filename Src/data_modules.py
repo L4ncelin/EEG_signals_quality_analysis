4f6891cb0f03227f455090901e3e46e58e794e51
Lancelin Poulet <p.lancelin09@laposte.net>
2024-12-30 18:04:26 +0100

    Visualization graph updated; NMF data preparation

--- Src/data_modules.py
+++ Src/data_modules.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 from scipy.signal import butter, lfilter
 import matplotlib.pyplot as plt
+from concurrent.futures import ProcessPoolExecutor, as_completed
+from tqdm import tqdm
 
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning)
@@ -10,12 +12,20 @@ warnings.filterwarnings("ignore", category=UserWarning)
 from multitaper_toolbox.python.multitaper_spectrogram_python import multitaper_spectrogram
 
 
-def load_train_data():
+def load_train_data(set:int=0):
+    """Load training set 
+
+    Args:
+        set (int, optional): Training set in [0,1,2,3]. Defaults to 0.
+
+    Returns:
+        _type_: _description_
+    """
     ROOT_PATH = Path("../Data/train/")
     training_data = [(np.load(ROOT_PATH / f"data_{i}.npy"),np.load(ROOT_PATH / f"target_{i}.npy")) for i in range(4)]
 
-    train_data = training_data[1][0]
-    train_target = training_data[1][1]
+    train_data = training_data[set][0]
+    train_target = training_data[set][1]
 
     return train_data, train_target
 
@@ -74,17 +84,116 @@ def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
     y = lfilter(b, a, data)
     return y
 
-def plot_signals_analysis(signal:pd.DataFrame, target_signal:pd.DataFrame, fs:int=250, window_size:int=2, duration:int=3600, channel:int=0, start_time:int=0):
+def get_multitaper(signal:pd.DataFrame, fs:int=250, duration:int=3600, channel:int=0, start_time:int=0):
+    """Return the multitaper matrix.
+
+    Args:
+        signal (pd.DataFrame): Real signal
+        fs (int, optional): Sampling rate. Defaults to 250Hz.
+        duration (int, optional): Duration we want to see. Defaults to 3600 seconds.
+        channel (int, optional): Channel to use. Defaults to 0.
+        start_time (int, optional): Starting time. Defaults to 0 seconds.
+
+    Returns:
+        np.ndarray : Multitaper matrix as np.array
+    """
+    # Indexs
+    start_idx_data = int(start_time * fs)
+    end_idx_data = start_idx_data + int(duration * fs)
+
+    # Signal
+    data = signal[channel, start_idx_data:end_idx_data]
+
+    # Multitaper
+    spect, _, _ = multitaper_spectrogram(data, fs, frequency_range=[0,30], plot_on=False, verbose=False)
+    multitaper_data = 10 * np.log10(spect)
+
+    return multitaper_data
+
+def get_multitaper_data(fs:int=250, nb_sets:int=4, nb_channels:int=5):
+    """Return all multitapers matrix.
+
+    Args:
+        fs (int, optional): Sampling rate. Defaults to 250Hz.
+        nb_sets (int, optional): Number of sets. Defaults to 4.
+        nb_channels (int, optional): Number of channels. Defaults to 5.
+
+    Returns:
+        list[np.ndarray]: Multitapers
+    """
+    multitaper_data = []
+    x=1
+    for i in range(nb_sets):
+        train_data, _ = load_train_data(set=i)
+        for c in range(nb_channels):
+            print("Iteration {}/{}".format(x, nb_sets*nb_channels))
+            x+=1
+            multitaper_data += [get_multitaper(train_data, fs, duration=int(train_data.shape[1])/fs, channel=c)]
+    
+    return multitaper_data
+
+def process_channel(args):
+    """Parralel task, getting the multitaper matrix of one set of one channel.
+
+    Args:
+        args (_type_): arguments of the task function
+
+    Returns:
+        np.ndarray: multitaper data
+    """
+    train_data, fs, duration, c, idx = args
+    result = get_multitaper(train_data, fs, duration, channel=c)
+    return idx, result
+
+def parallel_multitaper(nb_sets:int=4, nb_channels:int=5, fs:int=250):
+    """Return all multitapers matrix using parralel computation.
+
+    Args:
+        nb_sets (int, optional): Number of sets. Defaults to 4.
+        nb_channels (int, optional): Number of channels. Defaults to 5.
+        fs (int, optional): Sampling rate. Defaults to 250Hz.
+
+    Returns:
+        list[np.ndarray]: Multitapers
+    """
+    tasks = []
+    index = 0
+
+    for i in range(nb_sets):
+        train_data, _ = load_train_data(set=i)
+        duration = int(train_data.shape[1]) / fs
+        for c in range(nb_channels):
+            tasks.append((train_data, fs, duration, c, index))  # Préparer les arguments pour chaque tâche
+            index += 1
+
+    multitaper_data = [None] * len(tasks)  # Créer une liste vide de la bonne taille
+
+    # Exécution parallèle avec barre de progression
+    with ProcessPoolExecutor() as executor:
+        # Soumettre toutes les tâches
+        futures = {executor.submit(process_channel, task): task for task in tasks}
+
+        # Affichage de la barre de progression
+        for future in tqdm(as_completed(futures), total=len(tasks), desc="Calcul multitaper"):
+            idx, result = future.result()
+            multitaper_data[idx] = result  # Placer le résultat au bon endroit dans la liste
+
+    return multitaper_data
+
+
+
+def plot_signals_analysis(signal:pd.DataFrame, target_signal:pd.DataFrame, fs:int=250, window_size:int=2, duration:int=3600, channel:int=0, start_time:int=0, set:int=0):
     """Plot Spectrogram signal, aligned with real signal and target signal. We can parameter the duration and the start time.
 
     Args:
         signal (pd.DataFrame): Real signal
         target_signal (pd.DataFrame): Target signal
         fs (int, optional): Sampling rate. Defaults to 250Hz.
-        window_size (int, optional): target window size. Defaults to 2 seconds.
-        duration (int, optional): duration we want to see. Defaults to 3600 seconds.
-        channel (int, optional): channel to use. Defaults to 0.
-        start_time (int, optional): starting time. Defaults to 0 seconds.
+        window_size (int, optional): Target window size. Defaults to 2 seconds.
+        duration (int, optional): Duration we want to see. Defaults to 3600 seconds.
+        channel (int, optional): Channel to use. Defaults to 0.
+        start_time (int, optional): Starting time. Defaults to 0 seconds.
+        set (int,optional): Training set. Defaults to 0.
     """
 
     # ----------------------------------- Data ----------------------------------- #
@@ -110,24 +219,25 @@ def plot_signals_analysis(signal:pd.DataFrame, target_signal:pd.DataFrame, fs:in
 
     # ---------------------------------- Graphs ---------------------------------- #
     fig, axs = plt.subplots(3, 1, figsize=(18,9))
-
+    
     cax = axs[0].imshow(multitaper_data, aspect="auto", vmin=-6, vmax=30, cmap="jet", extent=[0, t_data[-1], 30, 0])
     axs[0].invert_yaxis()
-    axs[0].set_ylabel("Spectrogram")
-    axs[0].set_xticklabels([int(i) for i in np.linspace(start_time , duration + start_time, 8)]) # To have the index of the real time we are studying
-    #fig.colorbar(cax, ax=axs[0])
+    axs[0].set_ylabel("Frequencies (Hz)")
 
     axs[1].plot(t_data, data)
     axs[1].set_xlim(0,t_data[-1])
-    axs[1].set_xticklabels([int(i) for i in np.linspace(start_time , duration + start_time, 8)])
     axs[1].set_ylabel("Signal amplitude")
 
     axs[2].plot(t_target, target_data)
     axs[2].set_xlim(0,t_target[-1])
-    axs[2].set_xticklabels([int(i) for i in np.linspace(start_time , duration + start_time, 8)])
     axs[2].set_xlabel("Time (seconds)")
     axs[2].set_ylabel("Defect classification")
 
-    fig.suptitle('Signals analysis for {} seconds'.format(int(t_data[-1])), fontsize=15)
+    cbar = fig.colorbar(cax, ax=axs, orientation='vertical', fraction=0.02, pad=0.04)
+    cbar.set_label('Power (dB)', rotation=270, labelpad=20)
+
+    fig.suptitle('Signals analysis for {} seconds in channel n°{} of training set n°{}'.format(int(t_data[-1]), channel, set), fontsize=15)
 
     plt.show()
+
+    return spect, stimes, sfreqs
